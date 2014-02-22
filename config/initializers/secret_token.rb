9317431f431b57fea82cce47a075439b216a8a53
Nworah Ayogu <nayogu@gmail.com>
2014-02-22 18:58:54 +0000

    LOTR first commit hbs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseAppHbs::Application.config.secret_key_base = '04147be3e9952f82dfa0bd53f9cab67abc8963da6536c7b5784d4e684f8a2007d8eae3406f5b7a2155797a78a8186247a18d73e20d235b082b4aeefdcd38370f'
